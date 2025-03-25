ba1919ce4da56e71db631ab508ff1452138553a3
SidW111 <111293278+SidW111@users.noreply.github.com>
2025-03-25 19:42:49 +0530

    listing all the todos

--- index.js
+++ index.js
@@ -35,6 +35,15 @@ program.command("add")
         console.log("todo added succesfully")
     })
 
+
+program.command("list")
+    .description("list all the todos")
+    .argument("<file>","to list all the tasks")
+    .action((file)=>{
+        const data= fs.readFileSync(file,"utf-8")
+        console.log(JSON.parse(data))
+    })
+
 program.command("remove")
     .description("to remove a todo")
     .argument("<Todo_Title>", "title of the todo to be removed")
@@ -48,6 +57,28 @@ program.command("remove")
             console.log("todo removed Successfully")
         }
     }) 
+
+program.command("mark")
+    .description("mark the todo as done")
+    .argument("<todo_title", "Enter the title to mark as done")
+    .action((todoTitle) =>{
+        let todos = readTodos()
+        let todoFound = false
+        todos = todos.map((todo) =>{
+            if(todo.Title === todoTitle ){
+                todo.Done = true;
+                todoFound=true
+            }
+            return todo;
+        })
+        if(todoFound){
+        writeTodos(todos)
+        console.log("todo Marked succefully")
+        }else{
+            console.log("todo Not Found")
+        }
+    })
+
 
 
 program.parse()
